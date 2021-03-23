f526a64a65e6ba154aa9a45767273d99eef3d771
Iván L. Murillo <diskman199@gmail.com>
2021-03-23 11:31:35 -0600

    Update Send_Email.vb
    Now working with condition

--- CAR/Send_Email.vb
+++ CAR/Send_Email.vb
@@ -1,49 +1,57 @@
 Sub Send_Email()
     On Error GoTo ErrHandler
-    'Get data' 
+    'Get data'
     Worksheets("Summary").Activate
 
-    NumRows = Range("B3", Range("B3").End(xlDown)).Rows.Count
+    LastUpdate = Range("F2")
+    CurrentDate = Date
     
-    Dim html
-    html = "<!DOCTYPE html><html><body>"
-    html = html & "<div style=""font-family:'Segoe UI', Calibri, Arial, Helvetica; font-size: 14px; max-width: 768px;"">"
-    html = html & "Buen dia, Calidad <br /><br /> Envio el estado actual de la bitacora de CAR <br />"
-    html = html & "Here is sheet1 data:<br /><br />"
-    html = html & "<table style='border-spacing: 0px; border-style: solid; border-color: #ccc; border-width: 0 0 1px 1px;'>"
-
-    Range("B3").Select
-
-    For I = 1 To NumRows
-        html = html & "<tr>"
-        Header = Range("B" & I + 2)
-        Value = Range("C" & I + 2)
-
-        html = html & "<td style='padding: 10px; border-style: solid; border-color: #ccc; border-width: 1px 1px 0 0;'>" & Header & "</td>"
-        html = html & "<td style='padding: 10px; border-style: solid; border-color: #ccc; border-width: 1px 1px 0 0;'>" & Value & "</td>"
-
-        html = html & "</tr>"
-    Next    
-
-    html = html & "</table></div></body></html>"
-
-    Dim objOutlook As Object
-    Set objOutlook = CreateObject("Outlook.Application")
-    
-
-    Dim objEmail As Object
-    Set objEmail = objOutlook.CreateItem(olMailItem)
-
-    With objEmail
-        .to = "i.lopez@mx.interplex.com"
-        .Subject = "Estado Bitacora CAR"
-        .Body = html
-        .Send
-    End With
-    
-    ' CLEAR.
-    Set objEmail = Nothing:    Set objOutlook = Nothing
+    If IsEmpty(LastUpdate) Or DateDiff("d", LastUpdate, CurrentDate) >= 1 Then
+        NumRows = Range("B3", Range("B3").End(xlDown)).Rows.Count
         
+        Dim html
+        html = "<!DOCTYPE html><html><body>"
+        html = html & "<div style=""font-family:'Segoe UI', Calibri, Arial, Helvetica; font-size: 14px; max-width: 768px;"">"
+        html = html & "Buen día, equipo. <br /><br /> Envio el estado actual de la bitácora de CAR <br />"
+        html = html & "Aquí las próximas fechas de documentacion y documentación vencida<br /><br />"
+        html = html & "<table style='border-spacing: 0px; border-style: solid; border-color: #ccc; border-width: 0 0 1px 1px;'>"
+    
+        Range("B3").Select
+    
+        For I = 1 To NumRows
+            html = html & "<tr>"
+            Header = Range("B" & I + 2)
+            Value = Range("C" & I + 2)
+    
+            html = html & "<td style='padding: 10px; border-style: solid; border-color: #ccc; border-width: 1px 1px 0 0;'>" & Header & "</td>"
+            html = html & "<td style='padding: 10px; border-style: solid; border-color: #ccc; border-width: 1px 1px 0 0;'>" & Value & "</td>"
+    
+            html = html & "</tr>"
+        Next
+    
+        html = html & "</table></div></body></html>"
+    
+        Dim objOutlook As Object
+        Set objOutlook = CreateObject("Outlook.Application")
+        
+    
+        Dim objEmail As Object
+        Set objEmail = objOutlook.CreateItem(olMailItem)
+    
+        With objEmail
+            .To = "i.lopez@mx.interplex.com"
+            .Subject = "Estado Bitácora CAR"
+            .HTMLBody = html
+            .Send
+        End With
+        
+        Set objEmail = Nothing:    Set objOutlook = Nothing
+        Range("F2").Value = CurrentDate
+        
+        Exit Sub
+    End If
+
 ErrHandler:
-    MsgBox Err.Description
+    Range("F2").Value = LastUpdate
+
 End Sub
