d482b93a008d76f238082e6b1208084b8772afc9
Iván L. Murillo <diskman199@gmail.com>
2021-08-24 09:55:52 -0500

    Update Validation.vb
    More validations

--- SharePoint/DMR/Validation.vb
+++ SharePoint/DMR/Validation.vb
@@ -2,10 +2,8 @@ Sub SaveAlert()
     On Error GoTo DebugFunction
     Dim I As Integer
     Length = Worksheets("Validation").Range("A2", Worksheets("Validation").Range("A2").End(xlDown)).Rows.Count
-    MsgBox Length
     
     For I = 2 To Length + 1
-    
         Reference = Worksheets("Validation").Range("A" & I)
         Value = Range(Reference)
         
@@ -14,15 +12,41 @@ Sub SaveAlert()
         
         If Value = "" Then
             CallNotification Title, Content, Reference
-            Exit Sub
+            'Exit Sub
+            Exit For
         End If
-        
     Next I
     
-    MsgBox "Hola!!!"
+    Contention = Range("filledActions")
+    
+    If Contention <> 42 Then
+        CallNotification "FALTA LLENADO", "Por favor, llene toda la tabla de acciones de contención, si un valor no aplica, escriba N/A", "A17"
+        Exit Sub
+    End If
+    
+    If Not (Range("production") Or Range("warehouse") Or Range("planning")) Then
+        CallNotification "FALTA AVISAR MOVIMIENTO DE MATERIALES", "Por favor, indique al menos a un área que se avisó del movimiento de materiales", "production"
+        Exit Sub
+    End If
+    
+    Risk = Range("riskLevel")
+    
+    Reasons = Range("noRiskReasons")
+    
+    If Risk = "No" And Reasons = "" Then
+        CallNotification "FALTA LLENAR RAZONES", "Por favor, llene la razón y acciones de porqué no hay riesgo", "noRiskReasons"
+        Exit Sub
+    End If
+    
+    If Risk = "Medio" Or Risk = "Alto" Then
+        'Validación del plan de acción
+    End If
+    
+    MsgBox "Form valido"
+    
     Exit Sub
 DebugFunction:
-    MsgBox Reference
+    MsgBox "Error: " & Err.Description
 End Sub
 
 Sub CallNotification(Title, Content, Cell)
@@ -33,6 +57,7 @@ Sub CallNotification(Title, Content, Cell)
     If Response = vbOK Then
         Range(Cell).Select
     End If
+    
 EndFunction:
 End Sub
 
