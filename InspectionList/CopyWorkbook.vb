c4d08068363cc19e8b33c46e163a0ebf94daf850
Iván L. Murillo <diskman199@gmail.com>
2021-09-27 09:44:34 -0500

    Copy from one source to another

--- InspectionList/CopyWorkbook.vb
+++ InspectionList/CopyWorkbook.vb
@@ -1,29 +1,34 @@
 Sub OpenWorkbook(Filename As String)
-    MsgBox (Filename)
-    Application.ScreenUpdating = False
+    'On Error GoTo ErrHandler
+    A = ActiveWorkbook.Name  'Workbook destino
+    B = "Data"               'Hoja destino
     
+    Dim Destino As Worksheet
+    Dim Origen As Worksheet
     Dim src As Workbook
     
-    ' OPEN THE SOURCE EXCEL WORKBOOK IN "READ ONLY MODE".
+    Application.ScreenUpdating = False
+    MsgBox "Filename: " & Filename
+    
     Set src = Workbooks.Open(Filename, True, True)
+    Y = src.Name            'Workbook origen
+    Z = "Sheet1"            'Hoja origen
     
-    ' GET THE TOTAL ROWS FROM THE SOURCE WORKBOOK BY GETTING ALL ROWS IN COLUMN B.
-    Dim TotalRows As Integer
-    TotalRows = src.Worksheets("sheet1").Range("B1:B" & Cells(Rows.Count, "B").End(xlUp).Row).Rows.Count
     
-    MsgBox (TotalRows)
-   
-    ' COPY DATA FROM SOURCE (CLOSE WORKGROUP) TO THE DESTINATION WORKBOOK.
-    Dim i As Integer            ' COUNTER.
-    For i = 1 To TotalRows
-        Worksheets("Data").Range("B" & i).Formula = src.Worksheets("Sheet1").Range("B" & i).Formula
-    Next i
+    Set Origen = Workbooks(A).Worksheets(B)
+    Set Destino = Workbooks(Y).Worksheets(Z)
     
-    ' CLOSE THE SOURCE FILE.
-    src.Close False             ' FALSE - DON'T SAVE THE SOURCE FILE.
-    Set src = Nothing
+    Origen.Range("B2").Value = Destino.Range("B2")
+
     
-ErrHandler:
+    
+    'Close all
     Application.EnableEvents = True
     Application.ScreenUpdating = True
+    src.Close (False)
+    Set src = Nothing
+    Exit Sub
+ErrHandler:
+    MsgBox "Error: " & Err.Description
+    Exit Sub
 End Sub
